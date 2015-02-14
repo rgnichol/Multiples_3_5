d313a115d8e919614b2847724e08348dfe8d6831
Ryan <rgnichol@unca.edu>
2015-02-13 19:12:25 -0500

    Finished third solution using order 1 Gaussian solution with user entered ceiling.

--- multiples_3_5.cpp
+++ multiples_3_5.cpp
@@ -17,15 +17,23 @@ int main(int argc, char** argv) {
     
     int sum = 0;
     int n = 0;
+    int div3 = 0;
+    int div5 = 0;
+    int div15 = 0;
     
     cout << "Please enter ceiling: ";
     cin >> n;
     cout << "\n\n";
     
-    for (int i = 0; i < n; i++) {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }//for
+    n = n - 1;
+    
+    div3 = (n - (n % 3)) / 3;
+    div5 = (n - (n % 5)) / 5;
+    div15 = (n - (n % 15)) / 15;
+    
+    sum = 3 * (div3 * (div3 + 1))/2
+        + 5 * (div5 * (div5 + 1))/2
+        - 15 * (div15 * (div15 + 1))/2;
     
     cout << "Sum = " << sum << "\n";
     
